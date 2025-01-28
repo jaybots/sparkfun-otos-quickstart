c9e2695cf2273903eece03490b690d02f3339982
Jaybots - FTC team 6378 <tgill@ccsd.cc>
2025-01-28 12:50:40 -0500

    rr tuning start auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAuto.java
@@ -1,29 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
-import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.Trajectory;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.acmerobotics.roadrunner.TrajectoryBuilder;
-import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
@@ -42,7 +30,6 @@ public class RRAuto extends LinearOpMode {
         odo.setLinearScalar(1.0);
         sleep(100);
         odo.setAngularScalar(1.0);
-        DistanceSensor laser = hardwareMap.get(DistanceSensor.class, "laser");
         DcMotor lift = hardwareMap.get(DcMotor.class, "lift");
         DcMotor tilt = hardwareMap.get(DcMotor.class, "tilt");
         DcMotor hang = hardwareMap.get(DcMotor.class, "hang");
