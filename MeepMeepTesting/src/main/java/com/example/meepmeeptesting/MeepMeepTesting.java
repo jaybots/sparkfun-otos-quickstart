29bd37400c8b7a541dce0466c604925c5461c87d
Jaybots - FTC team 6378 <tgill@ccsd.cc>
2025-01-30 10:27:20 -0500

    meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,17 +14,15 @@ public class MeepMeepTesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(40, 50, Math.toRadians(180), Math.toRadians(180), 14)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(7, -34, Math.PI/2))
-                                .strafeTo(new Vector2d(24,-34))
-                                .splineToConstantHeading(new Vector2d(38,-10),Math.PI/2)
-                                .strafeTo(new Vector2d(48,-10))
-                                .lineTo(new Vector2d(48, -50))
-                                .lineTo(new Vector2d(48, -40))
-                                .turn(Math.PI)
-//                        .splineTo(new Vector2d(35.86, -32.11), Math.toRadians(83.07))
-//                        .splineTo(new Vector2d(35.86, -9.64), Math.toRadians(-35.22))
-//                        .splineTo(new Vector2d(42.98, -54.59), Math.toRadians(270.00))
-                                .build());
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(7, -32, Math.PI/2))
+                        .setTangent(-Math.PI/2)
+                        .splineToSplineHeading(new Pose2d(35,-31,-Math.PI/2),Math.PI/2)
+                        .splineToConstantHeading(new Vector2d(35,-21),Math.PI/2)
+                        .splineToConstantHeading(new Vector2d(47,-11),-Math.PI/2)
+                        .splineToConstantHeading(new Vector2d(47,-46),-Math.PI/2)
+                        .splineToConstantHeading(new Vector2d(47,-40),-Math.PI/2)
+                        .build());
+
 
 
 
