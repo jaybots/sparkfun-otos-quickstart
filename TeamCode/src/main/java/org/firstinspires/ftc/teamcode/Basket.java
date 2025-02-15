ba91aee340cd7e8cb42a5aa7d459627391a0ec2d
Jaybots - FTC team 6378 <tgill@ccsd.cc>
2025-02-14 22:22:04 -0500

    meepmeep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basket.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Trajectory;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
@@ -14,6 +15,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name = "Left (clip color then push yellow)")
 public class Basket extends LinearOpMode {
     RRHardware rr = new RRHardware();
+    double fast = 30;
+    double slow = 10;
 
     public void runOpMode() {
         rr.init(hardwareMap);
@@ -21,30 +24,29 @@ public class Basket extends LinearOpMode {
         Pose2d initialPose = new Pose2d(-7, -61, Math.toRadians(90));
         SparkFunOTOSDrive drive = new SparkFunOTOSDrive(hardwareMap, initialPose);
 
+
         TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
-                .lineToY(-31);
+                .lineToY(-30);
 
 
-        TrajectoryActionBuilder tab2 = drive.actionBuilder(new Pose2d(-7, -31,Math.PI/2 ))
-                .splineToConstantHeading(new Vector2d(-7,-38),Math.PI/2,new TranslationalVelConstraint(20))
-                .splineToConstantHeading(new Vector2d(-32,-38),Math.PI/2,new TranslationalVelConstraint(20))
-                .splineToConstantHeading(new Vector2d(-33,-21),Math.PI/2,new TranslationalVelConstraint(20))
-                .splineToConstantHeading(new Vector2d(-48,-8),-Math.PI/2,new TranslationalVelConstraint(20))
-                .splineToConstantHeading(new Vector2d(-48, -50), -Math.PI / 2,new TranslationalVelConstraint(40))
-                .splineToConstantHeading(new Vector2d(-48, -8), -Math.PI / 2,new TranslationalVelConstraint(20))
-                .splineToConstantHeading(new Vector2d(-56, -16), -Math.PI / 2,new TranslationalVelConstraint(20))
-                .splineToConstantHeading(new Vector2d(-56, -44), -Math.PI / 2,new TranslationalVelConstraint(40))
-                .splineToConstantHeading(new Vector2d(-56, -8), -Math.PI / 2,new TranslationalVelConstraint(20))
-                .splineToConstantHeading(new Vector2d(-60, -16), -Math.PI / 2,new TranslationalVelConstraint(20))
-                .splineToConstantHeading(new Vector2d(-60, -48), -Math.PI / 2,new TranslationalVelConstraint(40))
-                .splineToConstantHeading(new Vector2d(-48, -8), -Math.PI / 2,new TranslationalVelConstraint(20));
-
-
-
-
-        TrajectoryActionBuilder tab3 = drive.actionBuilder(new Pose2d(-48, -8,Math.PI/2 ))
-
-                .splineToSplineHeading(new Pose2d(-20,-8,Math.PI),0,new TranslationalVelConstraint(30));
+        TrajectoryActionBuilder tab2 = drive.actionBuilder(new Pose2d(-7, -30,Math.toRadians(90) ))
+                .setTangent(-Math.PI/2)
+                .splineTo(new Vector2d(-19,-35),Math.PI)
+                .splineTo(new Vector2d(-37,-8),Math.toRadians(85))
+                .setTangent(Math.toRadians(265))
+                .splineTo(new Vector2d(-42,-25),Math.toRadians(265))
+                .splineTo(new Vector2d(-44,-55),Math.toRadians(265))
+                .setTangent(Math.toRadians(85))
+                .splineTo(new Vector2d(-46,-8),Math.toRadians(85))
+                .setTangent(Math.toRadians(265))
+                .splineTo(new Vector2d(-58,-45),Math.toRadians(270))
+                .setTangent(Math.toRadians(90))
+                .splineTo(new Vector2d(-50,0),Math.toRadians(90))
+                .setTangent(Math.toRadians(270))
+                .splineToConstantHeading(new Vector2d(-60,-10),Math.toRadians(270))
+                .splineTo(new Vector2d(-60,-42),Math.toRadians(270))
+                .setTangent(Math.toRadians(90))
+                .splineTo(new Vector2d(-30,-9),Math.toRadians(0));
 
         while (!isStopRequested() && !opModeIsActive()) {
             sleep(100);
@@ -65,7 +67,6 @@ public class Basket extends LinearOpMode {
             //rr.tilt.setTargetPosition(0);
             //push three yellow blocks to corner
             Actions.runBlocking(new SequentialAction(tab2.build()));
-            Actions.runBlocking(new SequentialAction(tab3.build()));
             sleep(100);
             drive.setDrivePowers(new PoseVelocity2d(new Vector2d(-1,0 ),0));
             sleep(2000);
