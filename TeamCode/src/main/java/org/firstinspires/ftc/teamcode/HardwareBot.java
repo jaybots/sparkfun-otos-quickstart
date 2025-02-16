f9f58d8fb00d2b9042528565811bc8bafa9c7e68
Jaybots - FTC team 6378 <tgill@ccsd.cc>
2025-02-16 13:20:21 -0500

    meepmeep

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBot.java
@@ -0,0 +1,565 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class HardwareBot
+{
+
+    ElapsedTime timer = new ElapsedTime();
+    public double flspeed = 0;
+    public double frspeed = 0;
+    public double blspeed = 0;
+    public double brspeed = 0;
+    public DcMotor leftFront = null;
+    public DcMotor rightFront = null;
+    public DcMotor leftBack = null;
+    public DcMotor rightBack = null;
+    public DcMotorEx  lift = null;
+    public DcMotor  tilt = null;
+    public DcMotor  hang = null;
+    public CRServo spin1 = null;
+    public CRServo spin2 = null;
+    public Servo claw = null;
+    public ServoImplEx twist = null;
+    public DistanceSensor laser = null;
+    public SparkFunOTOS odo;
+    double spin1zero = 0.00;
+    double spin2zero = 0.00;
+    public double clawOpen = 0.3;
+    public double clawClosed = 0.56;
+    public double liftPower = 1;
+    public double tiltPower = 0.7;
+    public int liftPosition = 0;
+    public int tiltPosition = 0;
+    public int liftTarget = 0;
+    public int tiltTarget = 0;
+    public int floor = 2400;
+    public int maxHeight = 3500;
+    public double twistZero = 0.52;
+    public double twistPosition = 0.52;
+
+    HardwareMap hwMap  =  null;
+    SparkFunOTOS.Pose2D pos;
+
+
+    /* Constructor */
+    public HardwareBot(){
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+        odo = hwMap.get(SparkFunOTOS.class, "sensor_otos");
+        odo.calibrateImu();
+        sleep(100);
+        odo.setLinearUnit(DistanceUnit.INCH);
+        odo.setAngularUnit(AngleUnit.DEGREES);
+        odo.resetTracking();
+        sleep(100);
+        odo.setLinearScalar(1.0);
+        sleep(100);
+        odo.setAngularScalar(1.0);
+
+        // Define and Initialize Hardware
+        laser = hwMap.get(DistanceSensor.class, "laser");
+        leftFront = hwMap.get(DcMotor.class, "left_front");
+        rightFront = hwMap.get(DcMotor.class, "right_front");
+        leftBack = hwMap.get(DcMotor.class, "left_back");
+        rightBack = hwMap.get(DcMotor.class, "right_back");
+        lift = hwMap.get(DcMotorEx.class, "lift");
+        tilt = hwMap.get(DcMotor.class, "tilt");
+        hang = hwMap.get(DcMotor.class, "hang");
+        spin1 = hwMap.get(CRServo.class, "spin1");
+        spin2 = hwMap.get(CRServo.class, "spin2");
+        claw = hwMap.get(Servo.class, "claw");
+        twist = hwMap.get(ServoImplEx.class, "twist");
+
+
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        lift.setDirection(DcMotor.Direction.REVERSE);
+        tilt.setDirection(DcMotor.Direction.REVERSE);
+        hang.setDirection(DcMotor.Direction.FORWARD);
+        hang.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        tilt.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Set all motors to zero power
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        lift.setPower(0);
+        tilt.setPower(0);
+        hang.setPower(0);
+        spin1.setPower(spin1zero);
+        spin2.setPower(spin2zero);
+        claw.setPosition(clawClosed);
+
+
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        tilt.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hang.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        hang.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        tilt.setTargetPosition(0);
+        tilt.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        lift.setTargetPosition(0);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+    }
+
+
+    public void grabSpecimen(){
+        claw.setPosition(clawOpen);
+        sleep(400);
+        lift.setTargetPosition(500);
+        sleep(200);
+    }
+
+    /**
+     * Lowers lift and uses the laser to look for the chamber (bar), then releases specimen
+     */
+    public void releaseSpecimen(){
+        timer.reset();
+        lift.setPower(.75);
+        lift.setTargetPosition(0);
+        while (lift.getCurrentPosition()>1750 && timer.milliseconds()<200){
+        }
+        claw.setPosition(clawOpen);
+    }
+
+    /**
+     * Stops the intake wheels
+     */
+    public void spinStop(){
+        spin1.setPower(spin1zero);
+        spin2.setPower(spin2zero);
+    }
+    /**
+     * Spins the intake wheels to pull block in
+     */
+    public void spinIn(){
+        spin1.setPower(1);
+        spin2.setPower(-1);
+    }
+    /**
+     * Spins the intake wheels to push block out
+     */
+    public void spinOut(){
+        spin1.setPower(-1);
+        spin2.setPower(1);
+    }
+
+
+    /**
+     * Sets power to all drive wheels to zero
+     */
+    public void stopWheels(){
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+    }
+
+    /**
+     * Sets power to all drive wheels to the same value
+     * @param power as double
+     */
+    public void setWheels(double power){
+        leftFront.setPower(power);
+        rightFront.setPower(power);
+        leftBack.setPower(power);
+        rightBack.setPower(power);
+    }
+
+    /**
+     * Drives forward using time
+     * @param speed as double
+     * @param time in milliseconds
+     */
+    public void forwardTime(double speed, int time){
+
+        leftFront.setPower(speed);
+        rightFront.setPower(speed);
+        leftBack.setPower(speed);
+        rightBack.setPower(speed);
+        sleep(time);
+        stopWheels();
+    }
+
+    /**
+     * Drives backward using time
+     * @param speed (positive value)
+     * @param time in milliseconds
+     */
+    public void backTime(double speed, int time){
+
+        leftFront.setPower(-speed);
+        rightFront.setPower(-speed);
+        leftBack.setPower(-speed);
+        rightBack.setPower(-speed);
+        sleep(time);
+        stopWheels();
+    }
+
+    /**
+     * Drives forward using optical odometer
+     * @param dist in inches
+     * @param speed as double
+     */
+    public void forward(double dist, double speed){
+        resetOdo();
+        pos = odo.getPosition();
+        //p is proportional term for adjusting course
+        //px is drift left/right
+        //ph is drift in heading (yaw)
+        int px = 10;
+        int ph = 20;
+        flspeed = speed;
+        frspeed = speed;
+        blspeed = speed;
+        brspeed = speed;
+        double target = dist + pos.y;
+        while (pos.y < target){
+            flspeed = speed - pos.x/px + pos.h/ph;
+            brspeed = speed - pos.x/px - pos.h/ph;
+            frspeed = speed + pos.x/px - pos.h/ph;
+            blspeed = speed + pos.x/px + pos.h/ph;
+            pos = odo.getPosition();
+            leftFront.setPower(flspeed);
+            rightFront.setPower(frspeed);
+            leftBack.setPower(blspeed);
+            rightBack.setPower(brspeed);
+        }
+        stopWheels();
+    }
+
+
+
+
+
+    /**
+     * Drives right using optical odometer
+     * @param dist in inches
+     * @param speed as double
+     */
+    public void right(double dist, double speed){
+        resetOdo();
+        pos = odo.getPosition();
+        //p is proportional term for adjusting course
+        //px is drift forward/back
+        //ph is drift in heading (yaw)
+        int py = 50;
+        int ph = 50;
+        flspeed = speed;
+        frspeed = -speed;
+        blspeed = -speed;
+        brspeed = speed;
+        double target = dist + pos.x;
+        while (pos.x < target){
+            //adjustments made to each wheel to keep bot on course
+            flspeed = speed - pos.y/py + pos.h/ph;
+            brspeed = speed - pos.y/py - pos.h/ph;
+            frspeed = -speed - pos.y/py - pos.h/ph;
+            blspeed = -speed - pos.y/py + pos.h/ph;
+            pos = odo.getPosition();
+            leftFront.setPower(flspeed);
+            rightFront.setPower(frspeed);
+            leftBack.setPower(blspeed);
+            rightBack.setPower(brspeed);
+        }
+        stopWheels();
+    }
+
+    /**
+     * Drives right a set amount of time, then stops wheels
+     * @param time in milliseconds
+     * @param speed as double
+     */
+    public void rightTime(int time, double speed){
+        resetOdo();
+        pos = odo.getPosition();
+        //p is proportional term for adjusting course
+        //px is drift forward/back
+        //ph is drift in heading (yaw)
+        int py = 50;
+        int ph = 50;
+
+        flspeed = speed - pos.y/py + pos.h/ph;
+        brspeed = speed - pos.y/py - pos.h/ph;
+        frspeed = -speed - pos.y/py - pos.h/ph;
+        blspeed = -speed - pos.y/py + pos.h/ph;
+        pos = odo.getPosition();
+        leftFront.setPower(flspeed);
+        rightFront.setPower(frspeed);
+        leftBack.setPower(blspeed);
+        rightBack.setPower(brspeed);
+
+
+        leftFront.setPower(flspeed);
+        rightFront.setPower(frspeed);
+        leftBack.setPower(blspeed);
+        rightBack.setPower(brspeed);
+        sleep(time);
+        stopWheels();
+    }
+
+    /**
+     * Drives left using optical odometer, then stops wheels
+     * @param dist in inches
+     * @param speed as double
+     */
+    public void left(double dist, double speed){
+        resetOdo();
+        pos = odo.getPosition();
+        //p is proportional term for adjusting course
+        //px is drift forward/back
+        //ph is drift in heading (yaw)
+        int py = 50;
+        int ph = 50;
+        flspeed = -speed;
+        frspeed = speed;
+        blspeed = speed;
+        brspeed = -speed;
+        double target = pos.x - dist;
+        while (pos.x > target){
+            //adjustments made to each wheel to keep bot on course
+            flspeed = -speed - pos.y/py + pos.h/ph;
+            brspeed = -speed - pos.y/py - pos.h/ph;
+            frspeed = speed - pos.y/py - pos.h/ph;
+            blspeed = speed - pos.y/py + pos.h/ph;
+            pos = odo.getPosition();
+            leftFront.setPower(flspeed);
+            rightFront.setPower(frspeed);
+            leftBack.setPower(blspeed);
+            rightBack.setPower(brspeed);
+        }
+        stopWheels();
+    }
+    /**
+     * Drives left a set amount of time, then stops wheels
+     * @param time in milliseconds
+     * @param speed as double
+     */
+    public void leftTime(int time, double speed){
+
+        flspeed = -speed;
+        frspeed = speed;
+        blspeed = speed;
+        brspeed = -speed;
+        leftFront.setPower(flspeed);
+        rightFront.setPower(frspeed);
+        leftBack.setPower(blspeed);
+        rightBack.setPower(brspeed);
+        sleep(time);
+        stopWheels();
+    }
+
+    /**
+     * Drives back a set number of inches using optical odometer, then stops wheels
+     * @param speed as double
+     * @param dist in inches
+     */
+    public void back(double dist, double speed){
+        resetOdo();
+        pos = odo.getPosition();
+        //p is proportional term for adjusting course
+        //px is drift left/right
+        //ph is drift in heading (yaw)
+        int px = 10;
+        int ph = 20;
+        flspeed = -speed;
+        frspeed = -speed;
+        blspeed = -speed;
+        brspeed = -speed;
+        double target = pos.y - dist;
+        while (pos.y > target){
+            flspeed = -speed - pos.x/px + pos.h/ph;
+            brspeed = -speed - pos.x/px - pos.h/ph;
+            frspeed = -speed + pos.x/px - pos.h/ph;
+            blspeed = -speed + pos.x/px + pos.h/ph;
+            pos = odo.getPosition();
+            leftFront.setPower(flspeed);
+            rightFront.setPower(frspeed);
+            leftBack.setPower(blspeed);
+            rightBack.setPower(brspeed);
+        }
+        stopWheels();
+    }
+
+    /**
+     * Turns clockwise a set number of degrees then stops wheels
+     * @param turn degrees
+     */
+    public void cw(int turn){
+        resetOdo();
+        double speed = 0.2;
+        double headingChange = 0;
+        pos = odo.getPosition();
+        double startHeading = pos.h;
+        double currentHeading = pos.h;
+        while (headingChange < turn ){
+            if(headingChange > turn - 30){
+                speed=0.1;
+            }
+            leftFront.setPower(1*speed);
+            rightBack.setPower(-1*speed);
+            leftBack.setPower(1*speed);
+            rightFront.setPower(-1*speed);
+            pos = odo.getPosition();
+            currentHeading = pos.h;
+            if (Math.signum(startHeading) <Math.signum(currentHeading))
+                headingChange = 360 + startHeading - currentHeading;
+            else
+                headingChange = startHeading - currentHeading;
+        }
+        stopWheels();
+    }
+
+    /**
+     * Turns counterclockwise 180 degrees quickly then stops wheels
+     */
+    public void flip(){
+        resetOdo();
+        double speed = 1;
+        double currentHeading = 0;
+        while (currentHeading < 179 ){
+            pos = odo.getPosition();
+            if(currentHeading > 90){
+                speed=0.2;
+            }
+            leftFront.setPower(-1*speed);
+            rightBack.setPower(1*speed);
+            leftBack.setPower(-1*speed);
+            rightFront.setPower(1*speed);
+            pos = odo.getPosition();
+            currentHeading = Math.abs(pos.h);
+        }
+        stopWheels();
+    }
+
+    /**
+     * Turns right 90 degrees then stops wheels
+     */
+    public void right90(){
+        resetOdo();
+        double speed = 1;
+        double currentHeading = 0;
+        while (currentHeading < 89 ){
+            pos = odo.getPosition();
+            if(currentHeading > 15){
+                speed=0.2;
+            }
+            leftFront.setPower(1*speed);
+            rightBack.setPower(-1*speed);
+            leftBack.setPower(1*speed);
+            rightFront.setPower(-1*speed);
+            pos = odo.getPosition();
+            currentHeading = Math.abs(pos.h);
+        }
+        stopWheels();
+    }
+    /**
+     * Turns left 90 degrees then stops wheels
+     */
+    public void left90(){
+        resetOdo();
+        double speed = 1;
+        double currentHeading = 0;
+        while (currentHeading < 89 ){
+            pos = odo.getPosition();
+            if(currentHeading > 15){
+                speed=0.2;
+            }
+            leftFront.setPower(-1*speed);
+            rightBack.setPower(1*speed);
+            leftBack.setPower(-1*speed);
+            rightFront.setPower(1*speed);
+            pos = odo.getPosition();
+            currentHeading = Math.abs(pos.h);
+        }
+        stopWheels();
+    }
+
+
+    /**
+     * Turns counterclockwise a set number of degrees then stops wheels
+     * @param turn degrees
+     */
+    public void ccw(int turn){
+        resetOdo();
+        double speed = 0.2;
+        double headingChange = 0;
+        pos = odo.getPosition();
+        double startHeading = pos.h;
+        double currentHeading = startHeading;
+        while (headingChange < turn ){
+            if(headingChange > turn - 30){
+                speed=0.1;
+            }
+            leftFront.setPower(-1*speed);
+            rightBack.setPower(1*speed);
+            leftBack.setPower(-1*speed);
+            rightFront.setPower(1*speed);
+            pos = odo.getPosition();
+            currentHeading = pos.h;
+            if (Math.signum(startHeading) > Math.signum(currentHeading))
+                headingChange = 360 - startHeading + currentHeading;
+            else
+                headingChange = currentHeading - startHeading;
+
+        }
+        stopWheels();
+    }
+
+    /**
+     * Just waits a set amount of time
+     * All running motors will still be running
+     * @param x time in milliseconds
+     */
+    public void sleep(int x){
+        ElapsedTime s  = new ElapsedTime();
+        while (s.milliseconds()< x){
+        }
+    }
+
+    /**
+     * Sets the optical odometer to read 0 position (x and y) and zero heading
+     */
+    public void resetOdo(){
+        odo.setPosition(new SparkFunOTOS.Pose2D(0, 0, 0));
+        sleep(50);
+    }
+
+
+
+}
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mec.java
@@ -0,0 +1,264 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import java.lang.Math;
+
+
+@TeleOp(name="Mec")
+public class Mec extends LinearOpMode
+{
+    HardwareBot robot = new HardwareBot();
+
+
+    double liftControl = 0;
+    double twistControl = 0;
+    boolean grab = false;
+    boolean release = false;
+    ElapsedTime timer  = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+        SparkFunOTOS.Pose2D pos;
+        double speedFactor;
+        int maxExt = 1697;
+
+
+        // The while loop below runs until Play is pressed
+        // Current info is displayed
+        while (!isStarted() && !isStopRequested()){
+            //robot.claw.setPosition(robot.clawOpen);
+            pos = robot.odo.getPosition();
+            telemetry.addData("X coordinate", pos.x);
+            telemetry.addData("Y coordinate", pos.y);
+            telemetry.addData("Heading angle", pos.h);
+            telemetry.addData("tilt pos", robot.tilt.getCurrentPosition());
+            telemetry.addData("lift pos", robot.lift.getCurrentPosition());
+            telemetry.addData("tilt target", robot.tiltTarget);
+            telemetry.addData("lift target", robot.liftTarget);
+            telemetry.addData("lift amps", robot.lift.getCurrent(CurrentUnit.AMPS));
+            telemetry.update();
+            sleep(100);
+        }
+
+        robot.tilt.setPower(robot.tiltPower);
+        robot.lift.setPower(robot.liftPower);
+
+        while (opModeIsActive()) {
+
+            telemetry.addData("laser", robot.laser.getDistance(DistanceUnit.INCH));
+            telemetry.update();
+
+            //reset lift encoder if amps are high and it's in down position
+            if (robot.liftPosition < 100 && liftControl > -0.3 && robot.lift.getCurrent(CurrentUnit.AMPS) > 7) {
+                robot.lift.setPower(0);
+                robot.lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                robot.lift.setTargetPosition(0);
+                robot.lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.liftPosition = 0;
+                robot.liftTarget = 10;
+                robot.lift.setPower(robot.liftPower);
+            }
+
+            if (robot.tiltPosition > robot.floor/2){
+                robot.liftTarget = Math.min(robot.liftTarget,maxExt);
+                robot.claw.setPosition(robot.clawOpen);
+            }
+
+            robot.tilt.setTargetPosition(robot.tiltTarget);
+            robot.lift.setTargetPosition(robot.liftTarget);
+
+
+            robot.tiltPosition = robot.tilt.getCurrentPosition();
+            robot.liftPosition = robot.lift.getCurrentPosition();
+
+            liftControl = gamepad2.left_stick_y;
+            twistControl = gamepad2.right_stick_x;
+
+            //grab is set to true if right trigger is squeezed
+            //it activates all intake mechanisms (claw and spinner)
+            grab = gamepad2.right_trigger > 0.3;
+
+            //release is set to true if left trigger is squeezed
+            //it activates all outtake mechanisms (claw and spinner)
+            release = gamepad2.left_trigger > 0.3;
+
+            if (twistControl < -0.3 && robot.twistPosition < 1){
+                robot.twistPosition += 0.01;
+            }
+
+            else if (twistControl > 0.3 && robot.twistPosition > 0){
+                robot.twistPosition -= 0.01;
+            }
+
+            else if (gamepad2.right_stick_y < -0.3) robot.twistPosition = robot.twistZero;
+
+            robot.twist.setPosition(robot.twistPosition);
+
+            if (gamepad2.left_bumper){
+                robot.releaseSpecimen();
+                robot.liftTarget = 0;
+            }
+
+            if (gamepad1.x) {
+                robot.hang.setPower(1);
+            }
+            else if (gamepad1.y){
+                robot.hang.setPower(-1);
+                robot.tiltTarget = 400;
+            }
+            else robot.hang.setPower(0);
+
+            //going up by joystick
+            if (liftControl < -0.3 && (robot.liftTarget < robot.maxHeight || gamepad2.back)){
+                robot.liftTarget += 20;
+            }
+            //going down by joystick
+            else if (liftControl  > 0.3 ){
+                robot.liftTarget -=20;
+                //allow lift to go down below zero if 'back' is held
+                if (!gamepad2.back) robot.liftTarget = Math.max(0,robot.liftTarget);
+            }
+
+
+
+
+            //rotate tilt inward (up toward vertical)
+            if (gamepad2.dpad_down) {
+                robot.tiltTarget = Math.max(0,robot.tiltTarget - 15);
+            }
+            //rotate tilt away (down toward floor)
+            else if (gamepad2.dpad_up && (robot.tiltPosition < 200 || robot.liftPosition < maxExt *.9 )){
+                robot.tiltTarget = Math.min(robot.tiltTarget + 15, robot.floor);
+            }
+
+            if (grab && robot.tiltPosition < 300){
+                //grab with claw
+                robot.claw.setPosition(robot.clawClosed);
+                robot.spinIn();
+
+            }
+
+            if (grab && robot.tiltPosition >300){
+                //grab with wheels
+                robot.spinIn();
+            }
+
+            if (gamepad2.a){
+                robot.liftTarget = 0;
+            }
+
+
+            //lift various amounts
+            if (gamepad2.x){
+
+                if  (robot.liftPosition < 1900 && robot.tiltPosition < 100){
+                    robot.liftTarget = 2000;
+                    robot.twistPosition = 1;
+                }
+
+                else if (robot.tiltPosition < 100 && robot.liftPosition > 1900) {
+                    robot.twistPosition = robot.twistZero;
+                    robot.liftTarget = robot.maxHeight;
+                    robot.claw.setPosition(robot.clawOpen);
+                }
+            }
+            //tilt various amounts, timer enforces 0.5 second between changes
+            if (gamepad2.y && timer.milliseconds()>500){
+                timer.reset();
+                if  (robot.tiltPosition < 100 || robot.tiltPosition > robot.floor*.9){
+                    robot.tiltTarget = (int)(robot.floor*.75);
+                    release = true;
+                }
+                else {
+                    robot.tiltTarget = robot.floor - 20;
+                }
+            }
+
+            //tilt back to vertical
+            if (gamepad2.b ){
+                robot.tiltTarget = 0;
+            }
+
+            if (release){
+                //release
+                robot.spinOut();
+                robot.claw.setPosition(robot.clawOpen);
+            }
+
+            if (!release && !grab){
+                robot.spinStop();
+            }
+
+            //wheel control
+            double drive  = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double twist  = gamepad1.right_stick_x;
+            if (gamepad1.dpad_up) drive = 0.5;
+            if (gamepad1.dpad_down) drive = -0.5;
+            if (gamepad1.dpad_right) strafe = 0.5;
+            if (gamepad1.dpad_left) strafe = -0.5;
+            //Fine controls for movement on the ACCESSORIES side
+            if(gamepad2.dpad_right) strafe = 0.4;
+            if(gamepad2.dpad_left) strafe = -0.4;
+
+            //lower wheel power due to lift tilted down to ground
+            if (robot.tiltPosition > 400 || robot.liftPosition > robot.maxHeight*.9){
+                speedFactor = 0.5;
+            }
+            else
+                speedFactor = 0.8;
+
+            //speedFactor overrides
+            if (gamepad1.right_trigger > 0.3) speedFactor = 0.8;
+            if (gamepad1.left_trigger > 0.3) speedFactor = 0.5;
+
+            double[] speeds = {
+                    (drive + strafe + twist) * speedFactor,
+                    (drive - strafe - twist) * speedFactor,
+                    (drive - strafe + twist) * speedFactor,
+                    (drive + strafe - twist) * speedFactor
+            };
+
+            //check if any motor speeds are >1, and normalize if needed
+            double max = Math.abs(speeds[0]);
+            for (double speed : speeds) {
+                if (max < Math.abs(speed)) max = Math.abs(speed);
+            }
+            if (max > 1) {
+                for (int i = 0; i < speeds.length; i++) speeds[i] /= max;
+            }
+
+
+            //Set the drive motors to the correct powers.
+
+            robot.leftFront.setPower(speeds[0]);
+            robot.rightFront.setPower(speeds[1]);
+            robot.leftBack.setPower(speeds[2]);
+            robot.rightBack.setPower(speeds[3]);
+
+
+
+            if (gamepad2.right_bumper && robot.tiltPosition<200 && robot.liftPosition < 200 ){
+                robot.grabSpecimen();
+                robot.twistPosition = 1;
+                robot.flip();
+            }
+
+
+
+
+        } //ends while opMode is active loop
+    }  //brace ends opMode
+
+
+
+
+} //brace ends class
